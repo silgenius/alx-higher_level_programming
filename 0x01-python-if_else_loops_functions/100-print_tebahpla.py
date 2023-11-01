9d9ea881312283713c063f7c73c1dd3f8e4a098d
silgenius <olutademartin@gmail.com>
2023-10-31 19:38:36 -0700

    Fix pystylecode issue

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,9 +2,9 @@
 
 i = ord('Z')
 while i >= ord('A'):
-        if (i % 2 == 0):
-            i += 32
-        print("{}".format(chr(i)), end="")
-        if (i > ord('a')):
-                i -= 32
-        i -= 1
+    if (i % 2 == 0):
+        i += 32
+    print("{}".format(chr(i)), end="")
+    if (i > ord('a')):
+        i -= 32
+    i -= 1
