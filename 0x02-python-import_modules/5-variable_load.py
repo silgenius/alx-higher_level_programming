de75ee575be2c44eeb5f1e157622b593cc19fd35
silgenius <olutademartin@gmail.com>
2023-11-02 16:16:39 -0700

    Solve task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-import variable_load_5
+from variable_load_5 import a
 
 
 def print_num():
-    print("{:d}".format(variable_load_5.a))
+    print("{:d}".format(a))
 
 
 if __name__ == "__main__":
