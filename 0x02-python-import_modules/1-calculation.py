24ae51fb40765ff3eedfa9fe38cb2f6043ca9350
silgenius <olutademartin@gmail.com>
2023-11-02 09:25:08 -0700

    Solution to p-set 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import calculator_1
+
+
+def my_func():
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+
+
+if __name__ == "__main__":
+    my_func()
