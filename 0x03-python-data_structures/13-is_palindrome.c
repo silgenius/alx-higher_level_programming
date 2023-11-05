0b82a41b81dcf5e3af289b3f654ba7457b2d75f5
silgenius <olutademartin@gmail.com>
2023-11-05 07:02:53 -0800

    test for efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,47 +1,49 @@
 #include "lists.h"
 
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the linked list.
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
- * An empty list is considered a palindrome.
- */
 int is_palindrome(listint_t **head)
 {
-	int length, j, len;
-	listint_t *ptr, *last;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *next;
+    int is_palindrome = 1;
 
-	j = 0, length = 0;
-	ptr = *head;
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
-	if (ptr == NULL || ptr->next == NULL)
-		return (1);
-	while (ptr != NULL)
-	{
-		length++;
-		ptr = ptr->next;
-	}
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
 
-	len = length / 2;
-	ptr = *head;
+        next = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = next;
+    }
 
-	while (len != 0)
-	{
-		last = *head;
-		while (j < length - 1)
-		{
-			last = last->next;
-			j++;
-		}
+    if (fast != NULL)
+        slow = slow->next;
 
-		if (!(ptr->n == last->n))
-			return (0);
-		len--;
-		ptr = ptr->next;
-		length--;
-		j = 0;
-	}
+    while (prev != NULL && slow != NULL)
+    {
+        if (prev->n != slow->n)
+        {
+            is_palindrome = 0;
+            break;
+        }
+        prev = prev->next;
+        slow = slow->next;
+    }
 
-	return (1);
+    prev = NULL;
+    while (slow != NULL)
+    {
+        next = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = next;
+    }
+    *head = prev;
+
+    return is_palindrome;
 }
