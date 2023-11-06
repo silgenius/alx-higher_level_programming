3c8a89b9b575bd9d3123f62a33c372c40d2a25ee
silgenius <olutademartin@gmail.com>
2023-11-06 15:17:18 -0800

    fix issue

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -13,7 +13,7 @@ void print_python_list_info(PyObject *p)
 	while (i < length)
 	{
 		element = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
+		printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
 		i++;
 	}
 }
