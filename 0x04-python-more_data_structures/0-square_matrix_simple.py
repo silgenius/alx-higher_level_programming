2eab64f3c76774f4cf4cbb06ff255b09637362f0
silgenius <olutademartin@gmail.com>
2023-11-08 14:13:21 -0800

    Fix requirement error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix = []):
+def square_matrix_simple(matrix=[]):
     matrix = [list(map(lambda x: x * x, row)) for row in matrix]
     return (matrix)
