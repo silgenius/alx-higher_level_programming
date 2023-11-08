32c0db9a4f684212ade6702e61456609650e8cc5
silgenius <olutademartin@gmail.com>
2023-11-08 15:43:43 -0800

    Test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,6 @@
 def best_score(a_dictionary):
     high_score = 0
 
-    if a_dictionary is None:
-        return None
     for key in a_dictionary:
         if a_dictionary[key] > high_score:
             high_score = a_dictionary[key]
