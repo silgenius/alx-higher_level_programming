4c305588a29274ace5e8c5f14d24cc33efe8aab8
silgenius <olutademartin@gmail.com>
2023-11-08 17:46:49 -0800

    Fix requirement error:

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    my_dict = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     result = 0
     prev_value = 0
 
