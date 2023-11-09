695d26723c62920d3c7e70cf0312818deb3b41ae
silgenius <olutademartin@gmail.com>
2023-11-08 18:29:27 -0800

    Implement solution to task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list is None or len(my_list) = 0:
+    if my_list is None or len(my_list) == 0:
         return (0)
     result, value = 0, 0
     for tup in my_list:
