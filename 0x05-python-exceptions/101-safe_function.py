772c0a52b33ebac5150c2bbd5087abbe15e42f0b
silgenius <olutademartin@gmail.com>
2023-11-20 17:28:34 -0800

    Implement solution to task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,8 +5,8 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        result = fct(*args);
+        result = fct(*args)
     except Exception as err:
-        print("Exception: {}".format(err), file=stderr);
+        print("Exception: {}".format(err), file=stderr)
         return (None)
     return (result)
