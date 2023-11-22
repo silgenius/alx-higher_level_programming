6eb24a918e969dda8c84fb48ab40259796373ff5
silgenius <olutademartin@gmail.com>
2023-11-21 16:25:35 -0800

    Implement solution to Task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,17 +7,17 @@
 
 class Square:
     """
-        A simple class representing a square
-    
-        Attributes:
+    A simple class representing a square
+
+    Attributes:
         - size: The size of each side of the square.
     """
 
     def __init__(self, size):
-        self.__size = size
-    """
+        """
         Initializes a new square with the given side length.
 
         Parameters:
-        - size: The size of each side of the square.
-    """
+            - size: The size of each side of the square.
+        """
+    self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    A module for a class square
+"""
+
+
+class Square:
+    """
+    A class to represent a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square object.
+
+        Args:
+            size (int): The size of the Square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
