d5cdb24b15a7432e2df8cfe9dc15d2d6f741d044
silgenius <olutademartin@gmail.com>
2023-11-27 15:33:25 -0800

    Fix code to meet required unit test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -55,6 +55,10 @@ class Rectangle:
             value (int): The new width value.
         """
 
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -76,4 +80,9 @@ class Rectangle:
         Parameters:
             value (int): The new height value.
         """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
