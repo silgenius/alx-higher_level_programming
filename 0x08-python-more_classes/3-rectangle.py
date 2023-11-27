b3cca08bf06673e147ad15deb96937d6033b889a
silgenius <olutademartin@gmail.com>
2023-11-27 15:53:55 -0800

    Solution task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,7 +54,10 @@ class Rectangle:
         Parameters:
             value (int): The new width value.
         """
-
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -76,6 +79,10 @@ class Rectangle:
         Parameters:
             value (int): The new height value.
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
@@ -99,3 +106,20 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle using
+        # characters, forming a rectangular shape.
+
+        Returns:
+            str: The string representation of the rectangle
+        """
+
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        rectangle_str = ""
+        for _ in range(self.__height):
+            rectangle_str += "#" * self.__width + "\n"
+        return rectangle_str
