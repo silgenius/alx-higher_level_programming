91fe497b3de69f9135293487a15cf379966121c4
silgenius <olutademartin@gmail.com>
2023-12-04 20:38:51 -0800

    Solutions

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Contains the MyInt class. """
+
+
+class MyInt(int):
+    """ Inherits from int and has '==' and '!=' operators inverted. """
+
+    def __eq__(self, other):
+        """Compares with another value and if equal, returns false """
+        return True if self > other or self < other else False
+
+    def __ne__(self, other):
+        """Compares with another value and if equal, returns false """
+        return True if not (self > other) and not (self < other) else False
