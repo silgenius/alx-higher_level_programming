eaabbcaaee5d71c302b71d8e8b88c8f21609976d
silgenius <olutademartin@gmail.com>
2023-12-05 16:52:55 -0800

    Fix code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 from sys import argv
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-my_list = list(argv[1:])
-try:
-    file_content = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    save_to_json_file(my_list, "add_item.json")
-else:
-    file_content.extend(my_list)
-    save_to_json_file(file_content, "add_item.json")
+
+
+def add_save_json():
+    my_list = list(argv[1:])
+    try:
+        file_content = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        save_to_json_file(my_list, "add_item.json")
+    else:
+        file_content.extend(my_list)
+        save_to_json_file(file_content, "add_item.json")
+
+
+if __name__ == "__main__":
+    add_save_json()
