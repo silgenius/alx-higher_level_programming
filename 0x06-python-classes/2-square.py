5e39f5dea10f7e92bf053c70f7a92c3c774ee390
silgenius <olutademartin@gmail.com>
2024-04-05 10:55:18 -0700

    Fixed error in task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,14 +17,6 @@ class Square:
         Args:
             size (int): The size of the Square.
 
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
         """
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif (size < 0):
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
