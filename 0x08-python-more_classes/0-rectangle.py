9d300c2c54be67b52cef56ed49d11504e22faa83
silgenius <olutademartin@gmail.com>
2024-04-08 22:45:21 +0000

    Fix requirement error

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """
 This module defines a class Rectangle representing a geometric rectangle.
 It includes methods to set and retrieve the width and height of the rectangle.
