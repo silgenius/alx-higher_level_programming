dbda257007883e9b2c3a002b03a499a015788ea9
silgenius <olutademartin@gmail.com>
2024-05-22 16:55:33 -0700

    Solution to task

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+--  a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title AS title,
+tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title ASC, genre_id ASC
