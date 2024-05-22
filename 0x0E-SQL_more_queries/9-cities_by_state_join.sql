830bdb62a7d464b8e7324d1bc7f1a244a4e9a3d7
silgenius <olutademartin@gmail.com>
2024-05-22 15:56:10 -0700

    Implement solution to Task

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id,
+cities.name AS name,
+states.name AS name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
