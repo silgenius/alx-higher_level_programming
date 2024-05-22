8847156e5f9be83f997634e02e9d70c4ef3af18a
silgenius <olutademartin@gmail.com>
2024-05-22 14:53:11 -0700

    Refactore

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 --  a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
-	FOREIGN KEY(id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL,
+	state_id INT,
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
