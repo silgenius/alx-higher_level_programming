0f7bd99f4e935f8eca3f52e054265912e33ac348
silgenius <olutademartin@gmail.com>
2024-05-22 18:22:54 -0700

    Implement solution to task

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,5 @@ SELECT tv_genres.name AS name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows
-	WHERE title = 'Dexter')
+WHERE tv_show_genres.show_id = 8
 ORDER BY name ASC
