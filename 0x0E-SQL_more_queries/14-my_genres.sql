6e04c62aa381080c05f0f71ae2b50bbc75661edc
silgenius <olutademartin@gmail.com>
2024-05-22 18:36:09 -0700

    Refactor code in file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,7 @@ SELECT tv_genres.name AS name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = 8
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC
