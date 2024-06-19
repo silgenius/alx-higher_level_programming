305150092c0945da2e08b3a4685ed43ace83feed
silgenius <olutademartin@gmail.com>
2024-06-19 14:44:23 -0700

    Add solution to task

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// a script that prints “JavaScript is amazing
+
+const MyVar = 'JavaScript is amazing';
+console.log(MyVar);
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints a message depending of the number of arguments passed
+
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  console.log('Argument found');
+}
