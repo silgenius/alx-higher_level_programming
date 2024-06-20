b297292700876e098db983c1228ddfc04f500b8e
silgenius <olutademartin@gmail.com>
2024-06-19 18:21:12 -0700

    Refactor code to meet requirement

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a function that executes x times a function
+
+function (x, theFunction) {
+	while (x < 0)
+	{
+		theFunction;
+		x--;
+	}
+}
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,18 +3,18 @@
 // in the list of arguments.
 
 if (process.argv.length <= 3) {
-    console.log(0);
+  console.log(0);
 } else {
-    // Extract arguments, skip first two elements which are 'node' and the script name
-    const args = process.argv.slice(2).map(Number);
+  // Extract arguments, skip first two elements which are 'node' and the script name
+  const args = process.argv.slice(2).map(Number);
 
-    // Find the unique elements and sort them in descending order
-    const uniqueArgs = [...new Set(args)].sort((a, b) => b - a);
+  // Find the unique elements and sort them in descending order
+  const uniqueArgs = [...new Set(args)].sort((a, b) => b - a);
 
-    // Print the second largest element if there are at least two unique elements
-    if (uniqueArgs.length < 2) {
-        console.log(0);
-    } else {
-        console.log(uniqueArgs[1]);
-    }
+  // Print the second largest element if there are at least two unique elements
+  if (uniqueArgs.length < 2) {
+    console.log(0);
+  } else {
+    console.log(uniqueArgs[1]);
+  }
 }
