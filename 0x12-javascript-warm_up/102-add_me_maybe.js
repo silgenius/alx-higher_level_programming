47efa7b5ba8956ccc8320fb7f43615a8a2d5f43b
silgenius <olutademartin@gmail.com>
2024-06-19 19:27:00 -0700

    Implement solution to advance task

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // a function that executes x times a function.
 
-function addMeMaybe(number, theFunction) {
-    theFunction(number + 1);
+function addMeMaybe (number, theFunction) {
+  theFunction(number + 1);
 }
 
 module.exports = {
