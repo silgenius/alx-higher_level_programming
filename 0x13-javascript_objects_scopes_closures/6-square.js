b6e92bd8c55b2086ceef09482fa112637ef0ba9d
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-06-21 04:00:44 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
+  constructor(size) {
+    super(size, size); // Call the Rectangle constructor with size for both width and height
   }
 
-  charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X'; // Default to 'X' if c is undefined
+    }
+
+    for (let i = 0; i < this.height; i++) {
       let block = '';
-      for (let j = 0; j < this.size; j++) {
-        c ? block += c : block += 'X';
+      for (let j = 0; j < this.width; j++) {
+        block += c;
       }
       console.log(block);
     }
