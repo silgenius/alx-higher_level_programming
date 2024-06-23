039be693d3195c36e1b2213fb08f09f00636f147
silgenius <olutademartin@gmail.com>
2024-06-22 18:33:52 -0700

    refactor

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    query = "SELECT id, name FROM states WHERE name LIKE 'N%'"
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
