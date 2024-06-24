ae6eb5f5b6984f1ecb1e3b91bf6dc5b1b3d4725b
silgenius <olutademartin@gmail.com>
2024-06-23 20:57:52 -0700

    Implement solution to task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,7 +7,7 @@ database hbtn_0e_100_usa
 
 if __name__ == "__main__":
 
-    from relationship_state import State
+    from relationship_state import Base, State
     from relationship_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
