5d59b273ae5657625ccb532154edf6823dd34746
silgenius <olutademartin@gmail.com>
2024-06-27 18:47:59 -0700

    Implement solution to advance task

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,52 @@
-
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance Base"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+
+"""contains the class definition of a State"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Representation of a State"""
+    """contains the class definition of a City"""
+
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, unique=True,
-                nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all,delete-orphan")
+    id = Column(
+            Integer,
+            primary_key=True,
+            autoincrement=True,
+            unique=True,
+            nullable=False
+            )
 
+    name = Column(
+            String(128),
+            nullable=False
+            )
+
+    cities = relationship(
+            'City',
+            backref='state',
+            cascade='all, delete-orphan'
+            )
+
+
+if __name__ == "__main__":
+
+    import sys
+    from sqlalchemy import create_all
+
+    # Database connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create an engine and connect to the MySQL database
+    db_url = f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    # Create all tables in the database
+    Base.metadata.create_all(engine)
